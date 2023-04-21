813e7ceb7549c39dd228fab74afe19da95b33535
P N <82761006+petyaNikleva@users.noreply.github.com>
2023-04-21 14:32:18 +0300

    Add local storageMiddleware

--- src/store.js
+++ src/store.js
@@ -1,6 +1,30 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import reducers from "./reducers";
 
-const store = createStore(reducers);
+const applicationState = 'applicationState';
+
+const localStorageMiddleware = ({ getState }) => {
+  return (next) => (action) => {
+    const result = next(action);
+    localStorage.setItem(applicationState, JSON.stringify(
+      getState()
+    ));
+    return result;
+  };
+};
+
+const reHydrateStore = () => {
+  if (localStorage.getItem(applicationState) !== null) {
+    return JSON.parse(localStorage.getItem(applicationState))
+  }
+}
+
+const store = createStore(
+  reducers,
+  reHydrateStore(),
+  applyMiddleware(
+    localStorageMiddleware,
+  )
+)
 
 export default store;
